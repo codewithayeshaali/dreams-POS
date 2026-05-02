5c6c6e777817b7f621348eb5ef77b5f78f6795c7
Ayesha Ali <ayeshaali62823@gmail.com>
2026-05-02 11:03:36 +0500

    fix error on versel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,7 @@ import UserForgotPassword from "../features/auth/userForgotPassword";
 import UserEmailVerification from "../features/auth/userEmailVerification";
 import UserSuccess from "../features/auth/userSuccess";
 import UserTwoStepVerification from "../features/auth/userTwostepverification";
-import UserResetPassword from "../features/auth/xyz";
+import UserResetPassword from "../features/auth/userResetPassword";
 
 import EmailVerification from "../features/auth/email-verification";
 import ForgotPassword from "../features/auth/forgot-password";
